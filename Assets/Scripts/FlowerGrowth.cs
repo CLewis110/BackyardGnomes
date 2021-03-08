7334517e6aa156b138774631908cd4d49f3d32d1
Lewicv83 <lewicv83@gmail.com>
2021-03-08 15:39:06 -0500

    Create FlowerGrowth.cs

---
+++ Assets/Scripts/FlowerGrowth.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGrowth : MonoBehaviour
+{
+    public float timer = 0f;
+    public float growTime = 6f;
+    public float maxSize = 2f;
+
+    public bool isMaxSize = false;
+
+    void Start()
+    {
+        
+    }
+
+   /* private IEnumerator Grow()
+    {
+        Vector2 startSize = transform.localScale;
+        Vector2 maxScale = new Vector2(maxSize, maxSize);
+
+        do
+        {
+
+        }
+        while ();
+    } */
+}
