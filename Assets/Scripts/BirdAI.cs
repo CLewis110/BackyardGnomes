9ced408698a283263ad02ef341c423505b5ff0a3
Lewicv83 <lewicv83@gmail.com>
2021-03-09 12:46:36 -0500

    added enemy pathfinding

--- Assets/Scripts/BirdAI.cs
+++ Assets/Scripts/BirdAI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using Pathfinding;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -7,8 +8,98 @@ public class BirdAI : MonoBehaviour
 {
     public Transform target;
 
+    //Times updated per second
     public float updateRate = 2f;
 
+    //Caching
     private Seeker seeker;
     private Rigidbody2D rb;
+
+    //Calculated path
+    public Path path;
+
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+
+    public float nextWaypointDist = 3f;
+
+    private int currentWaypoint = 0;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(target == null)
+        {
+            Debug.Log("No player found.");
+            return;
+        }
+
+        //Creates 
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if(target == null)
+        {
+            //TODO: Search for player
+            yield return false;
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Path found. Any errors? " + p.error);
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(target == null)
+            return;
+
+        if (path == null)
+            return;
+        
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path.");
+            pathIsEnded = true;
+            return;
+        }
+        pathIsEnded = false;
+
+        //Direction to next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+
+        //Move AI
+        rb.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextWaypointDist)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
 }
